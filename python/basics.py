f024b0a212206b4966a1963f26029e662b843584
gyanAsh <gyanash36@protonmail.com>
2022-09-02 13:09:14 +0530

    Python basics completed

---
+++ python/basics.py
@@ -0,0 +1,21 @@
+print("whhhaattt!!")
+
+print(5//2) # first part of the decimal value
+print(5**2) # 5^2 -> 5*5
+print(5%2)  # remaining value after division
+
+test_text = 'random words blaaa'.replace('blaaa','haha')
+text_length = len(test_text) # len even includes white space
+print(text_length)
+
+print(abs(-210))  #returns +ve value of the input
+
+'''
+This is a multi line comment
+'''
+
+a=2;b=4 #two logical lines in one physical line
+
+c=10000+3\
++5      #two physical lines in one logical line using '\'
+print(c)
