a472fc4a5534170629b27cfc0e22d6948b17805c
gyanAsh <gyanash36@protonmail.com>
2022-09-14 22:36:05 +0530

    Added fixed framerate of 60fps for steady output

--- Games/astro_game.py
+++ Games/astro_game.py
@@ -3,6 +3,7 @@ import pygame,sys
 pygame.init()
 WINDOW_WIDTH,WINDOW_HEIGHT = 1280,720
 displaySurface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+clock = pygame.time.Clock()
 
 #import image
 spaceShip = pygame.image.load('./Assets/astro/ship.png').convert_alpha()
@@ -15,6 +16,10 @@ pygame.display.set_icon(spaceShip)
 gameFont = pygame.font.Font("./Assets/astro/subatomic.ttf",50)
 text_surf = gameFont.render('Space Astro', True ,'white')
 
+
+#spaceShip info:
+ship_y=500
+
 while True:
 
     for event in pygame.event.get():
@@ -23,9 +28,13 @@ while True:
             pygame.quit()
             sys.exit()
 
-    
+    # control framerate
+    clock.tick(60)
+
+    #updates per frame(loop)
+    ship_y -= 0.25
     displaySurface.fill((0,5,35))
-    displaySurface.blit(spaceShip,(300,500))
+    displaySurface.blit(spaceShip,(300,ship_y))
     displaySurface.blit(text_surf,(500,200))
 
     pygame.display.update()
